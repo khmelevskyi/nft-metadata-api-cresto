6bd8e55bb90359746c7e18f30913bccd632ae937
Yevhen Khmelevskyi <khmellevskyi.ye23@gmail.com>
2022-01-20 23:19:10 +0200

    added configs

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+import os
+import Config as config
 from flask import Flask
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", config.DevelopmentConfig)
+app.config.from_object(env_config)
 
 @app.route("/")
 def index():
-    return "Hello World! x33"
+    secret_key = app.config.get("SECRET_KEY")
+    return f"Hello World! x33 secret key : {secret_key}"
 
