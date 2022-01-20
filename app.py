4c95b37babbf210abc46bf5fdf5d4c00f6a2d6fc
Yevhen Khmelevskyi <khmellevskyi.ye23@gmail.com>
2022-01-20 23:31:11 +0200

    fix config

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
-import Config as config
 from flask import Flask
 
 app = Flask(__name__)
-env_config = os.getenv("APP_SETTINGS", config.DevelopmentConfig)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 
 @app.route("/")
