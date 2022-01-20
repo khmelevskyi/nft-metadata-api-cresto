999cad682da30e0193ec040450ed7b931610ec74
Yevhen Khmelevskyi <khmellevskyi.ye23@gmail.com>
2022-01-21 01:04:53 +0200

    added postgres base

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask import Flask
 
 app = Flask(__name__)
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import Result
 
 @app.route("/")
 def index():
     secret_key = app.config.get("SECRET_KEY")
     return f"Hello World! x33 secret key : {secret_key}"
+
+@app.route("/<name>")
+def hello_name(name):
+    return f"Hello {name}"
 
--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     DEBUG = False
     DEVELOPMENT = False
+    CSRF_ENABLED = True
     SECRET_KEY = os.getenv("SECRET_KEY", "default-key")
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 
 class ProductionConfig(Config):
     pass
