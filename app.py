aa53de2b00af005939b5adbfe69dee1c73057f3a
Yevhen Khmelevskyi <khmellevskyi.ye23@gmail.com>
2022-01-22 16:47:11 +0200

    added api for tokens metadata

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
+import json
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask import Flask, render_template, request
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,12 +15,52 @@ migrate = Migrate(app, db)
 
 from models import Result
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    secret_key = app.config.get("SECRET_KEY")
-    return f"Hello World! x33 secret key : {secret_key}"
+    errors = []
+    if request.method == "POST":
+        
+        title = request.form["title"]
+        print(title)
 
-@app.route("/<name>")
-def hello_name(name):
-    return f"Hello {name}"
+        try:
+            result = Result(title=title)
+            db.session.add(result)
+            db.session.commit()
+        except:
+            errors.append("Unable to add item to DB")
+            
+    return render_template("index.html", errors=errors)
+
+
+@app.route("/mint", methods=["GET", "POST"])
+def add_token():
+    errors = []
+    if request.method == "POST":
+        
+        title = request.form["title"]
+        print(title)
+
+        try:
+            result = Result(title=title)
+            db.session.add(result)
+            db.session.commit()
+        except:
+            errors.append("Unable to add item to DB")
+            
+    return render_template("index.html", errors=errors)
+
+
+@app.route("/tokens")
+def get_all_tokens():
+    tokens = db.session.query(Result).all()
+    tokens_json = [c.as_dict() for c in tokens]
+    return jsonify(tokens_json)
+
+
+@app.route("/tokens/<id>")
+def get_one_token(id):
+    token = db.session.query(Result).get(id)
+    print(token)
+    return token.as_dict()
 
--- models.py
+++ models.py
@@ -1,18 +1,16 @@
 from app import db
-from sqlalchemy.dialects.postgresql import JSON
 
 class Result(db.Model):
     __tablename__ = 'results'
 
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String())
-    result_all = db.Column(JSON)
-    result_no_stop_words = db.Column(JSON)
+    title = db.Column(db.String())
 
-    def __init__(self, url, result_all, result_no_stop_words):
-        self.url = url
-        self.result_all = result_all
-        self.result_no_stop_words = result_no_stop_words
+    def __init__(self, title):
+        self.title = title
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
+
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
