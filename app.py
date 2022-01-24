3577d0e91c96cc3024944bee910e004ebb3d8fa7
Yevhen Khmelevskyi <khmellevskyi.ye23@gmail.com>
2022-01-24 04:05:02 +0200

    added contract interface, finished api for cresto passes

--- app.py
+++ app.py
@@ -1,66 +1,93 @@
 import os
-import json
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask import Flask, render_template, request
 from flask import Flask
+from loguru import logger
+
+from contract_functions import mint
+
+
+logger.add(
+    "debug.log",
+    format="{time} {level} {message}\n",
+    level="DEBUG",
+    rotation="30 KB",
+    compression="zip",
+)
+
 
 app = Flask(__name__)
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+logger.debug(f"Started app successfully with next config: {app.config}")
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+from models import CrestoPass
 
-from models import Result
+logger.debug("Connected to db successfully")
+
+BSC_MAINNET = "https://bsc-dataseed1.binance.org"
+BSC_TESTNET = "https://data-seed-prebsc-1-s1.binance.org:8545"
+
+NAME = "CRESTO PASS"
+SYMBOL = "CRESTOPASS"
+IMAGE = "ipfs://QmcZW6yPYtyLMRPtQzWEzWnXg1VRaMBA784wjyg7GPo3WE"
+DESCRIPTION = "demo test nft token"
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    errors = []
-    if request.method == "POST":
-        
-        title = request.form["title"]
-        print(title)
-
-        try:
-            result = Result(title=title)
-            db.session.add(result)
-            db.session.commit()
-        except:
-            errors.append("Unable to add item to DB")
-            
-    return render_template("index.html", errors=errors)
+    return "CRESTO API"
 
 
-@app.route("/mint", methods=["GET", "POST"])
+@app.route("/cresto_passes/mint/", methods=["GET", "POST"])
 def add_token():
     errors = []
     if request.method == "POST":
         
-        title = request.form["title"]
-        print(title)
+        owner_id = request.form["owner_id"]
+        print(owner_id)
 
         try:
-            result = Result(title=title)
-            db.session.add(result)
+            if app.config["DEBUG"] == True:
+                token_id = mint(owner_id, BSC_TESTNET)
+            else:
+                token_id = mint(owner_id, BSC_MAINNET)
+            token = CrestoPass(
+                id=token_id,
+                name=NAME,
+                symbol=SYMBOL,
+                image=IMAGE,
+                owner_id=owner_id,
+                description=DESCRIPTION
+            )
+            db.session.add(token)
             db.session.commit()
         except:
+            logger.error(f"Errors happened: {errors}")
             errors.append("Unable to add item to DB")
             
     return render_template("index.html", errors=errors)
 
 
-@app.route("/tokens")
+@app.route("/cresto_passes/", methods=["GET"])
 def get_all_tokens():
-    tokens = db.session.query(Result).all()
+    tokens = db.session.query(CrestoPass).all()
     tokens_json = [c.as_dict() for c in tokens]
     return jsonify(tokens_json)
 
 
-@app.route("/tokens/<id>")
+@app.route("/cresto_passes/<id>/", methods=["GET"])
 def get_one_token(id):
-    token = db.session.query(Result).get(id)
+    token = db.session.query(CrestoPass).get(id)
     print(token)
-    return token.as_dict()
+    try:
+        return token.as_dict()
+    except AttributeError:
+        return "Sorry, token doesn't exists :("
 
--- models.py
+++ models.py
@@ -1,13 +1,22 @@
 from app import db
 
-class Result(db.Model):
-    __tablename__ = 'results'
-
+class CrestoPass(db.Model):
+    __tablename__ = 'cresto_passes'
+    
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String())
+    name = db.Column(db.String())
+    symbol = db.Column(db.String())
+    owner_id = db.Column(db.String())
+    image = db.Column(db.String())
+    description = db.Column(db.String())
 
-    def __init__(self, title):
-        self.title = title
+    def __init__(self, id, name, symbol, owner_id, image, description):
+        self.id = id
+        self.name = name
+        self.symbol = symbol
+        self.owner_id = owner_id
+        self.image = image
+        self.description = description
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
