3e96cd80e4bce5b1271757377d49ae26d622666d
Yevhen Khmelevskyi <khmellevskyi.ye23@gmail.com>
2022-01-28 18:36:15 +0200

    added default api page, token_one_error_page, improved design and ux, added mintedAt column, prevented post request resubmission

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-from flask import jsonify
+import datetime
+from flask import jsonify, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask import Flask
 from loguru import logger
 from dotenv import load_dotenv
@@ -39,30 +40,61 @@ BSC_MAINNET_CHAIN_ID = int(56)
 BSC_TESTNET = "https://data-seed-prebsc-1-s1.binance.org:8545"
 BSC_TESTNET_CHAIN_ID = int(97)
 
-NAME = "CRESTO PASS"
-CATEGORY = "cresto_pass"
-IMAGE = "ipfs://QmcZW6yPYtyLMRPtQzWEzWnXg1VRaMBA784wjyg7GPo3WE"
-DESCRIPTION = str(
-    "Cresto Pass is a game pass that gives the following bonuses:\n\n"+
-    "1. Gives you the opportunity to buy 5 chests at cresto.app at a 30% discount after launching the game.\n\n"+
-    "2. Gives you a greatly increased chance (75%) of participating in IDO at cresto.app\n\n"+
-    "3. Gives access to private restaurant auctions.\n\n"+
-    "4. Allows you to get 1 vote in DAO Cresto\n\n"+
-    "The only way to get a Cresto Pass is to win it in an airdrop."
-)
-
+gen_dict = {
+    "cresto_pass": {
+        "name": "CRESTO PASS",
+        "category": "cresto_pass",
+        "image": "ipfs://QmcZW6yPYtyLMRPtQzWEzWnXg1VRaMBA784wjyg7GPo3WE",
+        "description": (
+            "Cresto Pass is a game pass that gives the following bonuses:\n\n"+
+            "1. Gives you the opportunity to buy 5 chests at cresto.app at a 30% discount after launching the game.\n\n"+
+            "2. Gives you a greatly increased chance (75%) of participating in IDO at cresto.app\n\n"+
+            "3. Gives access to private restaurant auctions.\n\n"+
+            "4. Allows you to get 1 vote in DAO Cresto\n\n"+
+            "The only way to get a Cresto Pass is to win it in an airdrop."
+        )
+    },
+    "cresto_voucher": {
+        "name": "CRESTO VOUCHER",
+        "category": "cresto_voucher",
+        "image": "ipfs://Qmdqkp2c4ivyqcLJ5sFcTUGDMUpAuF1V56oMqhiouaRkb4",
+        "description": "Gives 30% off a single purchase at cresto.app"
+    }
+}
 
 @app.route("/api/", methods=["GET", "POST"])
 def index():
-    return "CRESTO API"
+    errors = []
+    if request.method == "POST":
+        tokenId = request.form["tokenId"]
+        try:
+            tokenId = int(tokenId)
+            return redirect(f"/api/cresto-nfts/{tokenId}/")
+        except Exception as e:
+            print(e)
+            error = "Wrong format! Enter integer tokenId i.e. 0"
+            return redirect(url_for(".fail_index", error=error))
+
+    try:
+        errors.append(request.args["error"])
+    except KeyError:
+        pass
+
+    return render_template("index.html", errors=errors)
+
+@app.route("/api/", methods=["GET"])
+def fail_index():
+    pass
 
 
 @app.route("/api/cresto-nfts/mint/", methods=["GET", "POST"])
 def add_token():
     errors = []
+    text = None
     if request.method == "POST":
-        
+
         owner_id = request.form["owner_id"]
+        nft_selected = request.form["cresto_nft"]
         print(owner_id)
         password = request.form["password"]
         if password == os.getenv("MINT_PASSWORD"):
@@ -75,26 +107,45 @@ def add_token():
                 logger.info(f"Minting token with tokenId {token_id}...")
                 token = CrestoPass(
                     id=token_id,
-                    name=NAME,
-                    category=CATEGORY,
-                    image=IMAGE,
+                    name=gen_dict[nft_selected]["name"],
+                    category=gen_dict[nft_selected]["category"],
+                    image=gen_dict[nft_selected]["image"],
                     owner_id=owner_id,
-                    description=DESCRIPTION
+                    description=gen_dict[nft_selected]["description"],
+                    mintedAt=datetime.datetime.now()
                 )
                 db.session.add(token)
                 db.session.commit()
-
-                text = f"Created new token with tokenId: {token_id}"
-                logger.info(text)
-                return render_template("index.html", errors=errors, text=text)
+                return redirect(url_for(".success_mint", tokenId=token_id))
             except Exception as e:
-                errors.append(f"Unable to add item to DB. Reason: {e}")
+                error = f"Unable to add item to DB. Reason: {e}"
                 logger.error(f"Errors happened: {errors}")
+                return redirect(url_for(".fail_mint", error=error))
         else:
-            errors.append("Wrong password!")
+            error = "Wrong password!"
             logger.error(f"Errors happened: {errors}")
-            
-    return render_template("index.html", errors=errors)
+            return redirect(url_for(".fail_mint", error=error))
+    
+    try:
+        tokenId = request.args["tokenId"]
+        text = f"Created new token with tokenId: {tokenId}"
+    except KeyError:
+        pass
+
+    try:
+        errors.append(request.args["error"])
+    except KeyError:
+        pass
+
+    return render_template("mint_page.html", errors=errors, text=text)
+
+@app.route("/api/cresto-nfts/mint/", methods=["GET"])
+def success_mint():
+    pass
+
+@app.route("/api/cresto-nfts/mint/", methods=["GET"])
+def fail_mint():
+    pass
 
 
 @app.route("/api/cresto-nfts/", methods=["GET"])
@@ -109,7 +160,7 @@ def get_one_token(id):
     token = db.session.query(CrestoPass).get(id)
     print(token)
     try:
-        return token.as_dict()
+        return jsonify(token.as_dict())
     except AttributeError:
-        return "Sorry, token doesn't exists :("
+        return render_template("one_token_error_page.html")
 
--- models.py
+++ models.py
@@ -9,17 +9,19 @@ class CrestoPass(db.Model):
     owner_id = db.Column(db.String())
     image = db.Column(db.String())
     description = db.Column(db.String())
+    mintedAt = db.Column(db.DateTime())
 
-    def __init__(self, id, name, category, owner_id, image, description):
+    def __init__(self, id, name, category, owner_id, image, description, mintedAt):
         self.id = id
         self.name = name
         self.category = category
         self.owner_id = owner_id
         self.image = image
         self.description = description
+        self.mintedAt = mintedAt
 
     def __repr__(self):
-        return '<id {}> <name {}> <category {}>'.format(self.id, self.name, self.category)
+        return '<id {}> <name {}> <category {}> <mintedAt {}'.format(self.id, self.name, self.category, self.mintedAt)
 
     def as_dict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
